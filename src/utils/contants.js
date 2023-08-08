37f4e8751d5df879692e766d71cf679b4c2e5af4
parmar1999 <121011071+parmar1999@users.noreply.github.com>
2023-08-08 11:03:13 +0530

    First Commit youtube

---
+++ src/utils/contants.js
@@ -0,0 +1,12 @@
+const GOOGLE_API_KEY = "AIzaSyAdjWeA_7tj2K5ewIaPH00xkz6J5mhIFMY";
+
+export const LIVE_CHAT_COUNT = 25;
+
+export const YOUTUBE_VIDEOS_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  GOOGLE_API_KEY;
+
+export const YOUTUBE_SEARCH_API =
+  "http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=";
+
+// Live Chat >>>> Infinite Scroll >>>>>> Pagination
